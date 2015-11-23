c72d95426e968747f5d361ae80aba0cfde808bf0
Andras Olasz <aolasz+github@gmail.com>
2015-11-23 12:51:44 +0100

    enter the commit message for your changes. Lines starting

--- src/icosaio.py
+++ src/icosaio.py
@@ -7,56 +7,28 @@ Created on 2013.10.18.
 @version: 2015.07.06.dev.1
 '''
 
+import subprocess
 import numpy as np
 from elements import ELEMENTS
 
+
 def getxyz(fname, exclude=['']):
-    print(fname)
-    with open(fname, 'r') as f:
-        numatoms = int(f.readline().strip())
-        geom = np.empty((numatoms, 4), dtype=np.float64)
-        f.readline() #comment line is discarded
-        for i in range(numatoms):
-            line = f.readline()
-            pieces = line.split()
-            symbol = pieces[0]
-            pieces[0] = ELEMENTS[symbol].number
-            geom[i] = [float(i) for i in pieces]
-        return geom
-
-def getxyz_v1(fname):
-    with open(fname, 'r') as f:
-        numatoms = int(f.readline().strip())
-        coords = np.empty((numatoms, 3), dtype=np.float64)
-        symbols = []
-        f.readline() #comment line is discarded
-        for i in range(numatoms):
-            line = f.readline()
-            pieces = line.split()
-            symbols.append(pieces[0])
-            coords[i] = [float(i) for i in pieces[1:4]]
-        return (symbols, coords)
-
-
-def writemesh(fname, verts, faces, edges=None):
-    with open(fname, 'w') as f:
-        part1 = '\n'.join([' '.join([str(v_) for v_ in vert]) for vert in verts])
-        part2 = '\n'.join([' '.join([str(f_) for f_ in face]) for face in faces])
-        f.write('verts\n' + part1 + '\nfaces\n' + part2)
-
-def savexlsx(**kwargs):
-    data = kwargs['data']
-    fname = kwargs['fname']
-    from xlsxwriter.workbook import Workbook
-    workbook = Workbook(fname)
-    worksheet = workbook.add_worksheet('worksheet1')
-    format1 = workbook.add_format()
-    format1.set_num_format('0.000')
-    # Write a 2D array.
-    array1 = np.arange(15).reshape(3, 5)
-    for row, data in enumerate(array1):
-        worksheet.write_row(row, 0, data)
-    # Write a 1D array.
-    array2 = np.array([6, 7, 8])
-    worksheet.write_column('A5', array2)
-    workbook.close()
+    #TODO: Openbabel should be used through pybel; however, no success installing/compiling
+    #  openbabel with python bindings in a conda environment.
+    proc = subprocess.Popen(["obabel", fname, "-oxyz"], stdout=subprocess.PIPE)
+    line = True
+    numatoms = False
+    while line:
+        line = proc.stdout.readline().decode()
+        if not numatoms:
+            i = 0
+            numatoms = int(line.strip())
+            geom = np.empty((numatoms, 4), dtype=np.float64)
+            proc.stdout.readline()  # comment line is discarded
+        elif i < numatoms:
+            words = line.split()
+            symbol = words[0]
+            words[0] = ELEMENTS[symbol].number
+            geom[i] = [float(i) for i in words]
+            i += 1
+    return geom
